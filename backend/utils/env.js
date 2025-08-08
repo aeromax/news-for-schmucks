6342dc108dcb54d84cfa815499bb1af2e7846e14
Max <maxpalmer@gmail.com>
2025-08-07 20:01:26 -0400

    test setup done

--- backend/utils/env.js
+++ backend/utils/env.js
@@ -1,9 +1,11 @@
 const requiredVars = ["NEWS_API_KEY", "OPENAI_API_KEY"];
-
+import dotenv from "dotenv";
+dotenv.config();
 export const env = {};
 
 for (const key of requiredVars) {
     const value = process.env[key];
+    
     if (!value) {
         console.error(`[ENV ERROR] Missing required environment variable: ${key}`);
         process.exit(1);
